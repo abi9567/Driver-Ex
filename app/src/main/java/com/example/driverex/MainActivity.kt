2b9f12f3b207e0ed086f53f22d538cb98e5fabbb
anu <anu@pixbit>
2022-04-27 13:27:53 +0530

    Started

---
+++ app/src/main/java/com/example/driverex/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.driverex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val id : TextView = findViewById(R.id.textView)
+    }
+}
